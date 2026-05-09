5c7fdf9ff18f907cbf508aed2f7b8c14aa92bec7
Hzx6 <2811409020@qq.com>
2026-05-09 21:39:13 +0800

    知识库搭建

---
+++ src/test/java/com/hzx/app/LoveAppTest.java
@@ -0,0 +1,50 @@
+package com.hzx.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员祥";
+        String answer = loveApp.doChat(message, chatId);
+        // 第二轮
+        message = "我想让另一半（强）更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半是谁";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        String message = "怎么加强亲密关系" ;
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+
+    @Test
+    void doChatWithRag() {
+        String chatId = UUID.randomUUID().toString();
+        String message = "婚后怎么加强亲密关系" ;
+        // 使用 RAG 知识库问答
+        String answer = loveApp.doChatWithRag(message, chatId);
+        Assertions.assertNotNull(answer);
+        System.out.println("RAG 回答: " + answer);
+    }
+}
